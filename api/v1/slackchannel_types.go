c8e9235672f0c99e4b4daeb618c74429a5217335
Mochamad Gufron <mgufronefendi@gmail.com>
2025-03-10 08:56:34 +0700

    chores: update status doc

--- api/v1/slackchannel_types.go
+++ api/v1/slackchannel_types.go
@@ -22,6 +22,8 @@ import (
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// Status is the available status the controller manager would observer the current pod status
 type Status string
 
 const (
